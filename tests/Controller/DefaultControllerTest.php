c979974c1652baf28b081226163d56e33dbcd8f1
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2022-08-01 11:02:10 +0200

    START2 - PHPUnit DataProvider
    - rework smoke test with provider and group

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -6,11 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndexPageIsSuccessful(): void
+    /**
+     * @dataProvider providePublicUrlsAndStatusCodes
+     * @group smoke
+     */
+    public function testPublicUrlIsSuccessful(string $url, int $statusCode): void
     {
         $client = static::createClient();
-        $client->request('GET', '/');
+        $client->request('GET', $url);
 
-        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame($statusCode);
+    }
+
+    public function providePublicUrlsAndStatusCodes(): \Generator
+    {
+        yield 'index' => ['/', 200];
+        yield 'contact' => ['/contact', 200];
+        yield 'book_index' => ['/book', 200];
+        yield 'book_show' => ['/book/3', 200];
+        yield 'hello' => ['/hello/Georges', 200];
+        yield 'toto' => ['/toto', 404];
     }
 }
