744656882b60f12a164c318bd9efa3f88c9aec61
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2022-08-03 15:04:18 +0200

    MASTER1 - Services Exercise
    - Add OMDbApiConsumer, OmdbMovieTransformer, MovieProvider
    - Fix controller and templates
    - Add env var

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Movie;
+use App\Provider\MovieProvider;
 use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/movie', name: 'app_movie_')]
@@ -19,11 +22,19 @@ class MovieController extends AbstractController
         ]);
     }
 
-    #[Route('/{!id<\d+>?1}', name: 'details')]
-    public function details(Movie $movie): Response
+    #[Route('/{!id<\d+>?1}', name: 'show')]
+    public function show(Movie $movie): Response
     {
         return $this->render('movie/details.html.twig', [
             'movie' => $movie,
+        ]);
+    }
+
+    #[Route('/title/{title}', name: 'details')]
+    public function details(string $title, MovieProvider $provider): Response
+    {
+        return $this->render('movie/details.html.twig', [
+            'movie' => $provider->getMovieByTitle($title),
         ]);
     }
 }
